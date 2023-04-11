60d66a12ad7a24392b471e5e973baa5930743bd2
bielskialeksi <92392918+bielskialeksi@users.noreply.github.com>
2023-04-11 14:48:38 +0200

    disolve enemy finish

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,34 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int enemyHP = 3;
+    private Renderer shaders = new Renderer();
+
+    private float dissolve;
     // Start is called before the first frame update
     void Start()
     {
-        
+        shaders = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (enemyHP <= 0)
+        if (enemyHP == 3)
         {
-            Destroy(gameObject);
+            dissolve = Mathf.Lerp(dissolve, 1f, 2f * Time.deltaTime);
+
+            shaders.material.SetFloat("_Boolean", 1);
+            shaders.material.SetFloat("_disolve", Mathf.Lerp(dissolve, 0, 2f * Time.deltaTime));
+            
+        }
+        if (enemyHP == 0)
+        {
+
+            dissolve = Mathf.Lerp(dissolve, 0f, 2f * Time.deltaTime);
+
+            shaders.material.SetFloat("_Boolean", 0);
+            shaders.material.SetFloat("_disolve", Mathf.Lerp(dissolve, 0, 2f * Time.deltaTime));
+            
         }
     }
 
