a158d923f269ff89668669f671a77eda5dfca26c
bielskialeksi <92392918+bielskialeksi@users.noreply.github.com>
2023-04-12 14:04:41 +0200

    enemy disolve

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,9 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int enemyHP = 3;
     public GameObject healthPack;
     // Start is called before the first frame update
+    [SerializeField] private Renderer[] shaders = new Renderer[0];
+    private float disolve = 0;
+
     void Start()
     {
         
@@ -15,13 +18,53 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
+        if (enemyHP != 0)
+        {
+            
+            if (disolve < 0.99f)
+            {
+                disolve = Mathf.Lerp(disolve, 1f, 2f * Time.deltaTime);
+                
+            }
+            else
+            {
+                disolve = 1;
+            }
+            foreach (Renderer r in shaders)
+            {
+                foreach (Material m in r.materials)
+                {
+                    
+                    m.SetFloat("_Boolean", 1);
+                    m.SetFloat("_disolvePerso", Mathf.Lerp(disolve, 0, 2f * Time.deltaTime));
+                }
+                
+            }
+
+        }
+
         if (enemyHP <= 0)
         {
-            if (Random.Range(1,2) == 1)
+            disolve = Mathf.Lerp(disolve, 0f, 2f * Time.deltaTime);
+            if (disolve > 0.01f)
             {
-                Instantiate(healthPack, transform.position + Vector3.up, transform.rotation);
+                foreach (Renderer r in shaders) {
+                    foreach (Material m in r.materials)
+                    {
+                        m.SetFloat("_Boolean", 0);
+                        m.SetFloat("_disolvePerso", Mathf.Lerp(disolve, 0, 2f * Time.deltaTime));
+                    }
+                }
             }
-            Destroy(gameObject);
+            if (disolve <= 0.01f)
+            {
+                if (Random.Range(1, 2) == 1)
+                {
+                    Instantiate(healthPack, transform.position + Vector3.up, transform.rotation);
+                }
+                Destroy(gameObject);
+            }
         }
     }
 
