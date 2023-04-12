50ed292dbf4cfdb931298caab7ce9d52070dbd1b
Mastrum1 <92383309+Mastrum1@users.noreply.github.com>
2023-04-12 11:08:51 +0200

    Revert

--- Assets/Scripts/Flare.cs
+++ Assets/Scripts/Flare.cs
@@ -1,25 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Flare : MonoBehaviour
 {
+    public Transform startPosition;
+    [SerializeField] int maxQuantity = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Rigidbody>().AddForce(0, 100, 0);
+        
     }
+
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y <= 0) 
-        {
-            Destroy(gameObject);
-        }
-        if (transform.position.y >= 18)
-        {
-            gameObject.GetComponent<Rigidbody>().drag = 10;
-        }
+        
     }
 }
