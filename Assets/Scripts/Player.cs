0dbe7b702d82dfb1c17a725922f30601aa86c02a
Hazmat11 <ebertrand@gaming.tech>
2023-04-13 17:04:07 +0200

    merge all works, shader working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -21,7 +22,11 @@ public class Player : MonoBehaviour
     {
         if (_currentHP > _HP)
         {
-            Debug.Log("test");
+
+        }
+        if (_HP <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 
